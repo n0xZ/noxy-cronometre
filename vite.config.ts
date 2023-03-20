60c022e1fbafb759231de66e6ece828b8d8ad3b7
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-03-19 21:26:01 -0300

    add pwa plugin with manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,43 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import Unocss from 'unocss/vite'
 import { presetUno } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact(), Unocss({ presets: [presetUno()] })],
+	plugins: [
+		preact(),
+		Unocss({ presets: [presetUno()] }),
+		VitePWA({
+			injectRegister: 'auto',
+			registerType: 'autoUpdate',
+			includeAssets: ['favicon.ico'],
+			manifest: {
+				name: 'Liuz',
+				short_name: 'Liuz',
+				description: 'Handle your tasks based an countdown',
+				theme_color: '#ffffff',
+				start_url: '/',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable',
+					},
+				],
+			},
+		}),
+	],
 })
