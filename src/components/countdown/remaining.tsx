cdf13c84b572e4cdd16d6c79ab385fc36ee1153e
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-03-22 22:11:18 -0300

    chore: add new icons related to resume/pause

--- src/components/countdown/remaining.tsx
+++ src/components/countdown/remaining.tsx
@@ -1,4 +1,4 @@
-import { h } from 'preact'
+import { MutableRef, Ref } from 'preact/hooks'
 
 type Props = {
 	results: {
@@ -6,24 +6,73 @@ type Props = {
 		remainingTime: () => number
 		status: 'IN_PROGRESS' | 'PAUSED' | 'STALE'
 	}
-
+	audioRef: Ref<HTMLAudioElement | undefined>
 	resumeCountdown: () => void
 	clearCountdown: () => void
 	pauseCountdown: () => void
 }
 
-function SandClockIcon() {
+function SandClockIcon(props: { hasFinished: boolean }) {
+	const baseClassStyles = 'h-12 w-12 self-center  duration-300'
+
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width="32"
 			height="32"
 			viewBox="0 0 24 24"
-			class="h-12 w-12 self-center rotate-180 duration-100"
+			class={`${baseClassStyles} ${props.hasFinished ? 'rotate-180' : null}`}
 		>
 			<path
 				fill="currentColor"
 				d="M6 2h12v6l-4 4l4 4v6H6v-6l4-4l-4-4V2m10 14.5l-4-4l-4 4V20h8v-3.5m-4-5l4-4V4H8v3.5l4 4M10 6h4v.75l-2 2l-2-2V6Z"
+			/>
+		</svg>
+	)
+}
+function PauseIcon() {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="32"
+			height="32"
+			viewBox="0 0 32 32"
+			class="h-6 w-6"
+		>
+			<path
+				fill="currentColor"
+				d="M12 8v16H8V8h4m0-2H8a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2zm12 2v16h-4V8h4m0-2h-4a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2z"
+			/>
+		</svg>
+	)
+}
+function ResumeIcon() {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="32"
+			height="32"
+			viewBox="0 0 24 24"
+			class="h-6 w-6"
+		>
+			<path fill="currentColor" d="M6 18V6h2v12H6Zm4 0l10-6l-10-6v12Z" />
+		</svg>
+	)
+}
+function ClearIcon() {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="32"
+			height="32"
+			viewBox="0 0 15 15"
+			class="h-6 w-6"
+		>
+			<path
+				fill="currentColor"
+				fill-rule="evenodd"
+				d="M4.854 2.146a.5.5 0 0 1 0 .708L3.707 4H9a4.5 4.5 0 1 1 0 9H5a.5.5 0 0 1 0-1h4a3.5 3.5 0 1 0 0-7H3.707l1.147 1.146a.5.5 0 1 1-.708.708l-2-2a.5.5 0 0 1 0-.708l2-2a.5.5 0 0 1 .708 0Z"
+				clip-rule="evenodd"
 			/>
 		</svg>
 	)
@@ -38,30 +87,33 @@ export function CountdownRemaining(props: Props) {
 		<section class="h-full w-full container mx-auto max-w-xl flex flex-col justify-center space-y-3">
 			<h2 class="text-center text-xl font-bold">{props.results.title}</h2>
 			<p class="text-center "> {countdownTimer}</p>
-			<SandClockIcon />
+			<SandClockIcon hasFinished={isActive} />
 			<aside class="w-full mt-20 flex flex-row justify-center items-center space-x-10 c-neutral-50">
 				{isActive ? (
 					props.results.status === 'IN_PROGRESS' ? (
 						<button
-							class="rounded-lg bg-neutral-400 p-3 w-32 hover:opacity-60 duration-100 ease-in-out"
+							class="flex flex-row justify-center items-center space-x-2 rounded-lg bg-neutral-400 p-3 w-32 hover:opacity-60 duration-100 ease-in-out"
 							onClick={props.pauseCountdown}
 						>
-							Pause
+							<PauseIcon />
+							<span> Pause</span>
 						</button>
 					) : (
 						<button
-							class="rounded-lg bg-neutral-400 p-3 w-32 hover:opacity-60 duration-100 ease-in-out"
+							class=" flex flex-row justify-center items-center space-x-2 rounded-lg bg-neutral-400 p-3 w-32 hover:opacity-60 duration-100 ease-in-out"
 							onClick={props.resumeCountdown}
 						>
-							Resume
+							<ResumeIcon />
+							<span>Resume</span>
 						</button>
 					)
 				) : null}
 				<button
-					class="rounded-lg bg-red-400 p-3 w-32 hover:opacity-60 duration-100 ease-in-out"
+					class="flex flex-row justify-center items-center space-x-2 rounded-lg bg-red-400 p-3 w-32 hover:opacity-60 duration-100 ease-in-out"
 					onClick={props.clearCountdown}
 				>
-					Reset
+					<ClearIcon />
+					<span>Reset</span>
 				</button>
 			</aside>
 		</section>
